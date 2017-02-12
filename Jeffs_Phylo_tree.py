eb732dde0979e4f048d6279478776f430b91717a
Jeffrey Silverman <jsilverman@juniper.net>
2017-02-11 23:59:30 -0800

    subtree is not working properly.  I think the problem is that I am not returning an empty tuple when I should be.  Also, there is a block in subtree that should be refactored

--- Jeffs_Phylo_tree.py
+++ Jeffs_Phylo_tree.py
@@ -19,6 +19,8 @@ Created on Wed Jul  6 17:55:31 2016
 
 from __future__ import print_function   # Makes the code work equally well on python 2 or python 3
 import sys
+import pprint
+import pdb
 
 
 """
@@ -103,7 +105,7 @@ We will later use this function to calibrate trees so we can better estimate dat
 
 """
 # Set this flag to True for debugging purposes.  Clear this flag for production.
-DEBUG_FLAG=False
+DEBUG_FLAG=True
 
 Groodies =  ( "X",
                 ("Y",
@@ -124,7 +126,8 @@ def leafCount ( tree ):
         print( "In leaf count at {}".format( tree[0] ), file=sys.stderr )
 # This is a test to make sure that I haven't done something stupid.  In general, when I find a bug, I fix it, and then
 # I add a test to make sure that it stays fixed.
-    assert len(tree) == 3, "In leafCount, at {}: the length of tree is {} but should be 3".format( tree[0], len(tree) )
+    assert len(tree) == 3, "In leafCount, at {}: the length of tree is {} but should be 3.  tree is {}".format( tree[0], len(tree), \
+                                                str( tree ) )
     if tree[1] != () and tree[2] != () :
         return leafCount ( tree[1]) + leafCount ( tree[2])
     elif tree[1] != () :
@@ -188,49 +191,77 @@ def subtree(node_name, tree):
 ('Z', ('E', (), ()), ('L', (), ()))
 
 """
-    if DEBUG_FLAG :         # Keep
+# Verify that were given a tuple
+    assert type(tree) == type( () ), "In subtree: the type of tree should be tuple, but is {}".format(str(type(tree)))
+# If we're given an empty tree, then return an empty tree
+    if len(tree) == 0 :
+        return ( () )
+    # This is a test to make sure that I haven't done something stupid.  In general, when I find a bug, I fix it, and then
+    # I add a test to make sure that it stays fixed.
+    assert len(tree) == 3, "In subtree, at {}: the length of tree is {} but should be 3.  tree is {}".format(tree[0],
+                                                                            len(tree), str(tree) )
+    if DEBUG_FLAG :
+        print("node_name=", node_name, "tree is ", tree, file=sys.stderr )
         if tree[1] != () and tree[2] != () :
             print ( "In subtree at node {}.  One of the children is {} and the other child is {}".format \
-                    ( tree[0], tree[1][0], tree[2][0] ))
+                    ( tree[0], tree[1][0], tree[2][0] ), file=sys.stderr )
         elif tree[1] != ()  :
             print ( "In subtree at node {}.  Child 1 is {} and there is no child 2".format \
-                    ( tree[0], tree[1][0] ))
+                    ( tree[0], tree[1][0] ), file=sys.stderr)
         elif tree[2] != ()  :
             print ( "In subtree at node {}.  There is no child 1 and child 2 is {} ".format \
-                    ( tree[0], tree[2][0] ))
+                    ( tree[0], tree[2][0] ), file=sys.stderr)
         else :
-            print ( "In subtree at node {}.  There are no children ".format ( tree[0], tree[2][0] ))
+            print ( "In subtree at node {}.  There are no children ".format ( tree[0]  ), file=sys.stderr )
+# THIS IS HORRENDOUS DEBUG CODE
+        if tree[0] == "Ben" :
+            print("in subtree at Ben.  Hitting a breakpoint.  HORRENDOUS DEBUG CODE")
+            print("node_name=", node_name, file=sys.stderr )
+            pp.pprint(tree)
+            pdb.set_trace()
+
+    try:
+# This block of code should be refactored to test to see if we found what we are looking first.
+        if tree[1] == () and tree[2] == ():
+    # this node has no children, so it has no subtrees, this tree is a leaf.  The node_name
+    # is in tree[0].  If this node is not the node we are looking for, then return an empty
+    # tuple, because the node_name was not found
+            if tree[0] != node_name :
+                print("At {}: there are no children and did not find {} so can not search further".format(tree[0], \
+                                node_name), file=sys.stderr)
+# Construct a leaf node
+            return ( ( tree[0], (), ()  )  )
+        elif tree[1] != () and tree[2] != ():
+            if tree[0] == node_name :
+    # We found our node.  Both tree[1] and tree[2] are both subtrees
+                return ( tree[0], subtree(node_name,tree[1]), subtree(node_name,tree[2]) )
+            else :
+                return ( (subtree(node_name, tree[1]), subtree(node_name, tree[2]) ) )
+        elif tree[1] != () :
+    # tree[1] is a subtree, tree[2] is a leaf
+            if tree[0] == node_name :
+    # Here, we are building a new node, with the name of the node, the subtree in child 1, an empty subtree in child 2
+                return ( tree[0], subtree(node_name, tree[1], ()) )
+            else :
+    # Return a subtree, which is a tree, but do not create a new node
+                return subtree(node_name, tree[1] )
+    # tree[2] is a subtree, tree[1] is a leaf
+        elif tree[2] != () :
+            if tree[0] == node_name :
+                return ( tree[0], (), subtree(node_name, tree[2] ) )
+            else :
+                return subtree(node_name, tree[2] )
+        else :
+            print("About to raise AssertionError", file=sys.stderr )
 
 
-    if tree[1] == () and tree[2] == ():
-# this node has no children, so it has no subtrees, this tree is a leaf.  The node_name
-# is in tree[0].  If this node is not the node we are looking for, then return an empty
-# tuple, because the node_name was not found
-        if tree[0] == node_name :
-            return ( tree[0], subtree(node_name, tree[1], subtree(node_name, tree[2] ) ) )
-        else :
-            print ("At {}: there are no children so can not search further".format(tree[0]), file=sys.stderr )
-    elif tree[1] != () :
-# tree[1] is a subtree, tree[2] is a leaf
-        if tree[0] == node_name :
-# Here, we are building a new node, with the name of the node, the subtree in child 1, an empty subtree in child 2
-            return ( tree[0], subtree(node_name, tree[1], ()) )
-        else :
-# Return a subtree, which is a tree, but do not create a new node
-            return subtree(node_name, tree[1], ())
-# tree[2] is a subtree, tree[1] is a leaf
-    elif tree[2] != () :
-        if tree[0] == node_name :
-            return ( tree[0], (), subtree(node_name, tree[2] ) )
-        else :
-            return subtree(node_name, (), tree[2] )
-    else :
-        if tree[0] == node_name :
-# Both tree[1] and tree[2] are both subtrees
-            return ( tree[0], subtree(node_name,tree[1]), subtree(node_name,tree[2]) )
-        else :
-            return subtree(node_name, tree[1]), subtree(node_name, tree[2])
-
+    except AssertionError as a:
+        print("in subtree at {}: one of the children raised AssertionError.  Child 1 is {}.  Child 2 is {}" ).format(\
+                tree[0], tree[1][0], tree[2][0] )
+# Raise the AssertionException.  This will force the caller to run its AssertionError handler and output the values at
+# its level
+        raise
+    raise AssertionError("Not sure how we got here, in subtree, but we did.\n  Tree is {}".format(str(tree)))
 
 
 # This is a unit test.  If this module is imported into another module, then this test will fail and the following software
@@ -238,35 +269,54 @@ def subtree(node_name, tree):
 # executed
 if __name__ == "__main__" :
 # Do some unit testing
-# Define my own test tree, which is different from but similar to Groodies
-    jeffs_tree =         ( "Alice",      # Alace's children are Ben and George
-          ("Ben",      # Ben has 2 children, Charles and David
-           ( "Charles", (), ()    # Charles has no children
-             ), \
-           ( "David", (),    # David has 1 son, Eric
-             ("Eric", (),     # Eric has a son, Fred.  Fred a second son, not because that means anything but it is a test case
-              ("Fred", (), ())       # Fred has no children
-              ),
-             () \
-            ), \
-           ), \
-          ( 'George', (), () )       # George is the son of Alice
-        )
+    pp = pprint.PrettyPrinter()
 
+# Define my own test tree, which is different from but similar to Groodies
+# Note that there are 4 test cases: no children, #1 child, #2 child, 2 children
+    fred = ("Fred", (), () )         # Fred has no childer
+    george = ("George", (), () )    # George has no children
+    eric = ("Eric", fred, () )      # Fred is Eric's son (first child)
+    david = ("David", (), eric )    # Eric is David's son (second child.  I know this is strange, but I need it for debugging
+    charles = ( "Charles", (), () ) # Charles has no children
+    ben = ( "Ben", charles, david)  # Ben has 2 children
+    alice = ( "Alice", ben, george )
+    jeffs_tree = alice              # Note that this is not a copy!  2 identifiers pointing to the same object
+#    jeffs_tree =         ( "Alice",      # Alace's children are Ben and George
+#          ("Ben",      # Ben has 2 children, Charles and David
+#           ( "Charles", (), ()    # Charles has no children
+#             ), \
+#           ( "David", (),    # David has 1 son, Eric
+#             ("Eric", (),     # Eric has a son, Fred.  Fred a second son, not because that means anything but it is a test case
+#              ("Fred", (), ())       # Fred has no children
+#              ),
+#             () \
+#            ), \
+#           ), \
+#          ( 'George', (), () )       # George is the son of Alice
+#        )
+    pp.pprint( jeffs_tree )
 
     def my_test ( actual, nominal, label):
         """This is a generic test subroutine.  actual is the result of the function.  nominal is the "correct" answer
         label is a message to print """
 
-        assert actual == nominal, "ERROR at {}: {} should be {}".format( label, actual, nominal)
-        print ( label + " {} should be {}".format( actual,  nominal ) )
-
+        if actual == nominal :
+            print( "CORRECT: {}".format(label) )
+        else :
+            if type(nominal) == type( () ) :
+                print("ERROR at {} is a tuple.  Nominal is:".format(label) )
+                pp.pprint(nominal)
+                print("Actual is:")
+                pp.pprint(actual)
+            else :
+                print ( "ERROR at {}: {} should be {}".format( label, actual,  nominal ) )
+        return ( actual, nominal )
 
     my_test ( leafCount(Groodies), 4, "Groodies" )
 
     my_test ( leafCount((5,(3,("A", (), ()),("B", (), ())),("C", (), ()))), 3, "Instructor provided" )
 
-    my_test ( leafCount ( jeffs_tree, nominal=3, label="Jeff's tree" ) )
+    my_test ( leafCount ( jeffs_tree ), nominal=3, label="Jeff's tree" )
 
 
 
@@ -276,6 +326,10 @@ if __name__ == "__main__" :
     my_test ( find('E',Groodies), True, "E in Groodies" )
 
 
-    my_test ( subtree( ('W', (), ()) ) )
+    my_test ( subtree( node_name='W', tree=(('W', (), () )) ), nominal=('W', (), () ), label="Testing subtree with W" )
+
+    my_test ( subtree( node_name="David", tree=jeffs_tree), david, "Testing subtree with david" )
+
+    my_test ( subtree( node_name="Ben", tree=jeffs_tree), ben, "Testing subtree with ben" )
 
 
